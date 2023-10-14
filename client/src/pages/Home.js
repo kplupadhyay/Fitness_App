9085247d4aaef8778d9cd71bae5a6bb12d47c62d
kplupadhyay <upadhyaykapil61@gmail.com>
2023-10-13 23:56:54 -0700

    various components are made

---
+++ client/src/pages/Home.js
@@ -0,0 +1,38 @@
+// import { Box } from "@mui/material";
+// import React, { useState } from "react";
+// import Exercises from "../components/Exercises";
+// import HeroBanner from "../components/HeroBanner.js";
+// import SearchExercises from "../components/SearchExercises";
+
+// export default function Home() {
+//   return (
+//     <Box>
+//       <HeroBanner/>
+//       <SearchExercises></SearchExercises>
+//       <Exercises></Exercises>
+//     </Box>
+//   );
+// }
+
+
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
+  );
+};
+
+export default Home;
